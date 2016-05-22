ba3bb81d444841e352abd1db11b426d01383c86b
TuringGunner <dingsong87@gmail.com>
2016-05-22 15:15:38 +0800

    Add files via upload
    check if files exist!

--- download.py
+++ download.py
@@ -1,6 +1,7 @@
 import requests
 import urllib
 import shutil
+import os.path
 
 r = requests.get('http://music.163.com/api/playlist/detail?id=335280')
 
@@ -10,9 +11,13 @@ music_number = len(arr)
 
 for i in range(music_number):
     name = arr[i]['name'] + '.mp3'
-    link = arr[i]['mp3Url']
-    urllib.urlretrieve(link, name)
-    shutil.move('/home/d/github/cloudmusic/' + name, '/home/d/Music')
-    print name + ' Download Complete.'
+    files = os.path.isfile('/home/d/Music/' + name)
+    if not files:
+        link = arr[i]['mp3Url']
+        urllib.urlretrieve(link, name)
+        shutil.move('/home/d/github/cloudmusic/' + name, '/home/d/Music')
+        print name + ' Download Complete.'
+    else:
+        print name + ' Exist!'
 
 print 'All Files Download Complete!'
